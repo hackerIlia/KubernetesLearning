c530a9c1be8ec2bec8f10be340b45b00a46db3e4
Ilia Marinov <marinovila78@gmail.com>
2025-07-04 11:53:57 +0300

    First excercise

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string out_str = string.Empty;
+
+for(int i = 0; i < 20; i++)
+{
+  char sym = Convert.ToChar(Random.Shared.Next(48, 122));
+  out_str += sym;
+}
+
+while (true)
+{
+  Console.WriteLine(DateTime.Now + ": " + out_str);
+  Thread.Sleep(5000);
+}
